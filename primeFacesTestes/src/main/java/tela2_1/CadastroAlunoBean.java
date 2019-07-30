6bdcfab2fd08c63556950ff6c03851ec0653caab
thiberius <heitorssx2@gmail.com>
2019-07-29 23:23:58 -0300

    Atividade prática iniciais
    O “commit” contem as seguintes atividades 1.1 à 2.3 implementadas, sendo elas referentes ao curso “Sistemas Comerciais Java EE com CDI, JPA e PrimeFaces”.

---
+++ primeFacesTestes/src/main/java/tela2_1/CadastroAlunoBean.java
@@ -0,0 +1,45 @@
+package tela2_1;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "MBcab")
+@ViewScoped
+public class CadastroAlunoBean implements Serializable {
+	public static final long serialVersionUID = 1L;
+
+	private String nome;
+	private String email;
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("-----------------------------");
+		System.out.println("////// MBcab initiated //////");
+		System.out.println("-----------------------------");
+	}
+	
+
+	public void Cadastrar() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("E-mail:" + this.email);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
