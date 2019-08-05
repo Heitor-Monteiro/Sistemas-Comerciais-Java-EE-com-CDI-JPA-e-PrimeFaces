e3856a9ce64c5bddc9225f45128adac8793fde40
thiberius <heitorssx2@gmail.com>
2019-08-04 22:48:34 -0300

    2.8. Entrada de data e hora com Calendar

---
+++ primeFacesTestes/src/main/java/c2_8/PerfilUsuarioBean.java
@@ -0,0 +1,54 @@
+package c2_8;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "MBpub3")
+@ViewScoped
+public class PerfilUsuarioBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nome;
+	private Date dataNacimento;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("------------------------------");
+		System.out.println("////// MBpub3 initiated //////");
+		System.out.println("------------------------------");
+	}
+
+	public void atualizar() {
+		System.out.println("Data nascimento:" + this.dataNacimento);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil atualizado!"));
+	}
+
+	public Date getDataHoje() {
+		return new Date();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNacimento() {
+		return dataNacimento;
+	}
+
+	public void setDataNacimento(Date dataNacimento) {
+		this.dataNacimento = dataNacimento;
+	}
+
+}
