bb3cd112e3ccf3ee6f734c3450141ecd4eca5fa5
thiberius <heitorssx2@gmail.com>
2019-09-16 12:05:38 -0300

    2.9. Caixa de listagem com SelectOneListbox
    Aplicou-se o desenvolvimento prático de:
    - utilização de componente SelectOneListbox do PrimeFaces;
    - utilização do "static" para carregar elementos na classe. Obs.: não confundir com a inicialização de uma ManagedBean, esse carregamento ocorre somente uma vez ao inicializar o sistema;
    - exemplo simples de conversor JSF;
    - Incluindo imagens em cada elemento do SelectOnrListbox, para isso, foi consultado valores de variáveis definidas com static, ou seja, que carregaram junto com a classe.

---
+++ primeFacesTestes/src/main/java/c2_9/PerfilUsuarioBean4.java
@@ -0,0 +1,71 @@
+package c2_9;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "MBpub4")
+@ViewScoped
+public class PerfilUsuarioBean4 implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public static final List<Interesse> INTERESSES = new ArrayList<>();
+	private String nome;
+	private String profissao;
+	private Interesse interesse;
+
+	static {
+		System.out.println("+++++++ static method +++++++");
+		INTERESSES.add(new Interesse("Esportes", "esportes"));
+		INTERESSES.add(new Interesse("Computação", "computacao"));
+		INTERESSES.add(new Interesse("Cinema", "cinema"));
+		INTERESSES.add(new Interesse("Leitura", "leitura"));
+	}
+
+	@PostConstruct
+	public void init() {
+		System.out.println("------------------------------");
+		System.out.println("////// MBpub4 initiated //////");
+		System.out.println("------------------------------");
+	}
+
+	public void atualizar() {
+		System.out.println("Profissão:" + this.profissao);
+		System.out.println("Interesse:" + this.interesse.getDescricao());
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil atualizado!"));
+	}
+
+	public List<Interesse> getInteresses() {
+		return INTERESSES;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	public Interesse getInteresse() {
+		return interesse;
+	}
+
+	public void setInteresse(Interesse interesse) {
+		this.interesse = interesse;
+	}
+}
